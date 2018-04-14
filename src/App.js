1dc03a4c80be25d3669c3e5ec5fd19825f0078ae
florlafuente <lafuente.florencia@gmail.com>
2018-04-14 16:03:53 -0300

    Add partido component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Album from './components/Album'
+import Partido from './components/Partido'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Album />
+        <Partido />
       </div>
     );
   }

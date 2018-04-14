ab3a4dafe439e63b1d7817c28cf400a0baa2dfc9
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2018-04-14 16:03:37 -0300

    Agrega routeo bàsico

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
-import Album from './components/Album'
+import Album from './components/Album';
+import Figurita from './components/Figurita';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 import './App.css';
+
+// const BasicExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/">Album</Link>
+//         </li>
+//       </ul>
+
+//       <hr />
+
+//       <Route exact path="/" component={Album} />
+//     </div>
+//   </Router>
+// );
+
+const AlbumDeFiguritas = () => (
+  <div className="Album">
+    <Album />
+  </div>
+)
+
+const Partido = () => (
+  <div className="Partido">
+    Partido
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Album />
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Album</Link>
+            </li>
+            <li>
+              <Link to="/partido">Partido</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={AlbumDeFiguritas} />
+          <Route exact path="/partido" component={Partido} />
+        </div>
+      </Router>
     );
   }
 }
